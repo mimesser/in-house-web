f3cc8d21a5f5501d1fc1903266f959e84b5ba222
cuteolaf <oliverlim818@gmail.com>
2018-05-29 08:11:27 +0200

    correctly use newly created config file

--- config.js
+++
@@ -1,2 +0,0 @@
-export const API_URL = process.env.NODE_ENV === 'production' ?
-   'http://in-house-dev-api.azurewebsites.net/api' : 'http://localhost:49706/api/';
---
+++ src/config.js
@@ -0,0 +1,2 @@
+export const API_URL = process.env.NODE_ENV === 'production' ?
+   'http://in-house-dev-api.azurewebsites.net/api' : 'http://localhost:49706/api';
