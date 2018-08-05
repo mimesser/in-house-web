151d208a3aaea75a017843eb37c29c811a6e4257
mimesser <michaelmesser9507@gmail.com>
2018-08-05 11:25:12 +0200

    added stuff

--- src/services/venue.js
+++ src/services/venue.js
@@ -11,3 +11,25 @@ export async function createVenue(body) {
       return err.data.message;
    }
 }
+
+export async function suggestEdit(body) {
+   try {
+      await api.post('notify/suggest-edit', body);
+      await get();
+      return null;
+   } catch (err) {
+      console.log(err, err.status, err.message, err.response, err.data);
+      return err.data.message;
+   }
+}
+
+export async function applyAsOwner(body) {
+   try {
+      await api.post('notify/apply-as-owner', body);
+      await get();
+      return null;
+   } catch (err) {
+      console.log(err, err.status, err.message, err.response, err.data);
+      return err.data.message;
+   }
+}
