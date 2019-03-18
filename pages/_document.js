7b525986110f508cfe0facb74c65c653eec91d81
mimesser <michaelmesser9507@gmail.com>
2019-03-18 18:59:10 +0200

    Basic VenueList.

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,9 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+          <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
+          <link rel="stylesheet" type="text/css" href="/static/modern-normalize.css" />
+          <link rel="stylesheet" type="text/css" href="/static/site.css" />
         </Head>
         <body>
           <Main />
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,63 @@
+import { Fragment, useState } from 'react';
+import axios from 'axios';
+import { Input } from '../components';
+import { debounce } from '../utils';
+import api from '../api';
+import VenueList from '../components/venue-list';
+
+function cancelSearchFunc(cancelSource) {
+  if (cancelSource) {
+    cancelSource.cancel('Start new search, stop active search');
+    console.log('cancel request done');
+  }
+}
+
+const getVenues = debounce(async (value, setVenues, setLoading, cancelSource, setCancelSource) => {
+  cancelSearchFunc(cancelSource);
+  const source = axios.CancelToken.source();
+  setCancelSource(source);
+  try {
+    const { data: venues } = await api.get('/venues', { params: { IdentifierFreeText: value }, cancelToken: source.token });
+    setCancelSource(null);
+    setVenues(venues);
+    setLoading(false);
+  } catch (err) {
+    // Probably cancelled.
+  }
+}, 0, { leading: true, trailing: true });
+
+
+
 function Home({ mode }) {
+  const [search, setSearch] = useState('');
+  const [venues, setVenues] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [cancelSource, setCancelSource] = useState(null);
+
+  function onSearchChange(value) {
+    setLoading(true);
+    getVenues(value, setVenues, setLoading, cancelSource, setCancelSource);
+    setSearch(value);
+  }
+
+
   return (
-    <main>
-      <div className="container">
-        <h1><strong>In</strong>House</h1>
-        <h3>Coming soon...</h3>
-        <h5>{mode}</h5>
-      </div>
-      
-      <style global jsx>{`
-        body {
-          font-family: sans-serif;
-          margin: 0;
-        }
-        main {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          height: 100vh;
-          background-color: rgb(250, 250, 250);
-        }
-        h1, h3 {
-          font-weight: 100;
-        }
-        h1 {
-          color: #aaa;
-        }
-        h1 strong {
-          color: #333;
-        }
-        h3 {
-          color: #333;
+    <Fragment>
+      {!search && (
+        <Fragment>
+          <h1>Let's find your house</h1>
+          <p>You can find your workplace by looking up its name or address.</p>
+        </Fragment>
+      )}
+      <Input type="search" placeholder="Search for name or address" value={search} onChange={onSearchChange} />
+      {loading && <div>Loading...</div>}
+      {!loading && <VenueList venues={venues} />}
+      <style jsx>{`
+        p {
+          width: 267px;
         }
       `}</style>
-    </main>
+    </Fragment>
   );
 }
 
