51428f06c74321516e2995dd7130e9e7ceced33d
cuteolaf <oliverlim818@gmail.com>
2019-03-20 19:14:43 +0200

    Convert VenueList to class.

--- pages/index.js
+++ pages/index.js
@@ -1,72 +1,76 @@
-import { Fragment, useState } from 'react';
+import { Fragment, Component } from 'react';
 import axios from 'axios';
 import { Input } from '../components';
-import { debounce } from '../utils';
 import api from '../api';
-import VenueList from '../components/venue-list';
+import VenueList from '../components/venue-list';  
 
-function cancelSearchFunc(cancelSource) {
-  if (cancelSource) {
-    cancelSource.cancel('Start new search, stop active search');
-    console.log('cancel request done');
+
+export default class Home extends Component {
+  state = {
+    search: '',
+    venues: [],
+    loading: false,
+    cancelSource: null, // Used with axios to cancel previous request.
+  }
+
+  setSearch = (search) => {
+    this.setState({ loading: true, search });
+    this.getVenues(search);
+  }
+
+  cancelSearchFunc = () => {
+    const { cancelSource } = this.state;
+    if (cancelSource) {
+      cancelSource.cancel('Start new search, stop active search');
+    }
+  }
+
+  getVenues = async (search) => {
+    this.cancelSearchFunc();
+    const source = axios.CancelToken.source();
+    this.setState({ cancelSource: source });
+    try {
+      const { data: venues } = await api.get('/venues', { params: { IdentifierFreeText: search }, cancelToken: source.token });
+      this.setState({ cancelSource: null, venues, loading: false });
+    } catch (err) {
+      // Probably cancelled.
+      // TODO: Simply canceling axios requests is not good enough.  We need to debounce the requests.
+    }
+  }
+
+  render() {
+    const { search, loading, venues } = this.state;
+    return (
+      <Fragment>
+        {!search && (
+          <Fragment>
+            <h1>Let's find your house</h1>
+            <p>You can find your workplace by looking up its name or address.</p>
+          </Fragment>
+        )}
+        <Input type="search" placeholder="Search for name or address" value={search} onChange={this.setSearch} />
+        {(() => {
+          if (!search) return null;
+          if (loading) return <div className="loading">Loading...</div>;
+          if (venues.length) return <VenueList venues={venues} />;
+          return <p className="no-houses">No current houses match your search.</p>
+        })()}
+        <style jsx>{`
+          .no-houses {
+            font-size: 24px;
+            line-height: 28px;
+            margin-top: 32px;
+          }
+          .loading {
+            margin-top: 24px;
+          }
+          @media screen and (max-width: 480px) {
+            p {
+              width: 267px;
+            }
+          }
+        `}</style>
+      </Fragment>
+    );
   }
 }
-
-const getVenues = debounce(async (value, setVenues, setLoading, cancelSource, setCancelSource) => {
-  cancelSearchFunc(cancelSource);
-  const source = axios.CancelToken.source();
-  setCancelSource(source);
-  try {
-    const { data: venues } = await api.get('/venues', { params: { IdentifierFreeText: value }, cancelToken: source.token });
-    setCancelSource(null);
-    setVenues(venues);
-    setLoading(false);
-  } catch (err) {
-    // Probably cancelled.
-  }
-}, 0, { leading: true, trailing: true });
-
-
-
-function Home({ mode }) {
-  const [search, setSearch] = useState('');
-  const [venues, setVenues] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [cancelSource, setCancelSource] = useState(null);
-
-  function onSearchChange(value) {
-    setLoading(true);
-    getVenues(value, setVenues, setLoading, cancelSource, setCancelSource);
-    setSearch(value);
-  }
-
-
-  return (
-    <Fragment>
-      {!search && (
-        <Fragment>
-          <h1>Let's find your house</h1>
-          <p>You can find your workplace by looking up its name or address.</p>
-        </Fragment>
-      )}
-      <Input type="search" placeholder="Search for name or address" value={search} onChange={onSearchChange} />
-      {loading && <div>Loading...</div>}
-      {!loading && <VenueList venues={venues} />}
-      <style jsx>{`
-        p {
-          width: 267px;
-        }
-      `}</style>
-    </Fragment>
-  );
-}
-
-Home.getInitialProps = () => {
-  // TODO: save in redux.
-  if (process.browser) {
-    return {};
-  }
-  return { mode: process.env.MODE };
-};
-
-export default Home;
