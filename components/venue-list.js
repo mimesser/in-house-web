41dc553f0c9d0b41f809645d2e8182ae7868f3b3
cuteolaf <oliverlim818@gmail.com>
2019-03-21 16:40:02 +0200

    Add VenueList cards.  Add CircleProgress.

--- components/venue-list.js
+++ components/venue-list.js
@@ -1,11 +1,126 @@
-export default function VenueList({ venues }) {
-   return venues.map(venue => (
-      <section key={venue.id}>
-         <div className="image" />
-         <div className="venue-details">
-            <h3>{venue.name}</h3>
-            <h3>{venue.venueInfo.address}, {venue.venueInfo.zipCode}</h3>
-         </div>
-      </section>
-   ));
+import { connect } from 'react-redux';
+import CircleProgress from './circle-progress';
+
+function getRandomScore() {
+   return (Math.random() * 10) + 1;
 }
+
+function getRandomInsiders() {
+   return Math.floor((Math.random() * 100) + 1);
+}
+
+function VenueList({ venues, industries }) {
+   return (
+      <section className="container">
+         <div className="row">
+            {venues.map((venue) => {
+               const industry = industries[venue.industryId];
+
+               return (
+                  <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3" key={venue.id}>
+                     <div className="card">
+                        <div className="image" />
+                        <div className="venue-details">
+                           <div className="industry">{industry && industry.name}</div>
+                           <div className="venue-name">{venue.name}</div>
+                           <div className="venue-address">{venue.venueInfo.address}<br />{venue.venueInfo.city}, {venue.venueInfo.state} {venue.venueInfo.zipCode}</div>
+                        </div>
+                        <div className="right">
+                           <CircleProgress score={getRandomScore()} />
+                           <div className="insiders">
+                              <i className="material-icons">
+                                 person
+                              </i>
+                              <span>{getRandomInsiders()}</span>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               );
+            })}
+         </div>
+         <style jsx>{`
+            section {
+               margin-top: 10px;
+            }
+            .card {
+               padding: 10px;
+               display: flex;
+               background-color: #fff;
+               margin: 10px 0;
+               height: 116px;
+               box-shadow: 0 1px 4px 0 rgba(0,0,0,0.08);
+               border-radius: 2px;
+               cursor: pointer;
+            }
+            .card:hover {
+               box-shadow: 0 2px 4px 0 rgba(0,0,0,0.12);
+            }
+            .image {
+               background-color: #E5E5E5;
+               height: 92px;
+               min-width: 92px;
+            }
+            .venue-details {
+               display: flex;
+               flex-direction: column;
+               overflow: hidden;
+               margin-left: 10px;               
+            }
+            .venue-name {
+               font-size: 18px;
+               color: #000000;
+               font-weight: bold;
+               white-space: nowrap;
+               overflow: hidden;
+               text-overflow: ellipsis;
+               margin: 5px 0;
+            }
+            .venue-address {
+               font-size: 12px;
+            }
+            .industry {
+               color: #9B9B9B;
+               font-size: 12px;
+            }
+            .right {
+               margin-left: auto;
+               display: flex;
+               flex-direction: column;
+               align-items: center;
+               justify-content: space-evenly;
+            }
+            .insiders i {
+               border: 1px solid #929DA7;
+               height: 16px;
+               width: 16px;
+               font-size: 12px;
+               border-radius: 50%;
+               text-align: center;
+               color: #929DA7;
+            }
+            .insiders {
+               color: #929DA7;
+            }
+            .insiders span {               
+               margin-left: 4px;
+               font-size: 12px;
+            }
+         `}
+         </style>
+      </section>
+   );
+}
+
+function mapStateToProps({ aggregate }) {
+   return {
+      industries: !aggregate
+         ? {}
+         : aggregate.industries.reduce((res, industry) => {
+            res[industry.id] = industry;
+            return res;
+         }, {}),
+   };
+}
+
+export default connect(mapStateToProps)(VenueList);
--- pages/_app.js
+++ pages/_app.js
@@ -1,64 +1,66 @@
-import React from 'react'
-import App, { Container } from 'next/app'
-import { Provider } from 'react-redux'
-import withReduxStore from '../lib/with-redux-store'
+import App, { Container } from 'next/app';
+import { Provider } from 'react-redux';
+import withReduxStore from '../lib/with-redux-store';
 import Header from '../components/header';
 import api, { setAuthorization } from '../api';
 
 class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {}
+   static async getInitialProps({ Component, ctx }) {
+      let pageProps = {};
 
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
-
-
-    return { pageProps }
-  }
-
-  async componentDidMount() {
-    try {
-      const cachedToken = localStorage.getItem('in-house/token');
-
-      if (process.env.NODE_ENV !== 'development' || !this.props.reduxStore.getState().aggregate) {
-        if (cachedToken) {
-          setAuthorization(cachedToken);
-        }
-
-        const { data: aggregate } = await api.get('aggregate', { method: 'GET' });
-
-        setAuthorization(aggregate.userId);
-
-        this.props.reduxStore.dispatch(({ type: 'SET_AGGREGATE', aggregate }));
+      if (Component.getInitialProps) {
+         pageProps = await Component.getInitialProps(ctx);
       }
-    } catch(err) {
-      // TODO:
-      throw err;
-    }
-  }
 
-  render () {
-    const { Component, pageProps, reduxStore } = this.props
 
-    return (
-      <Container>
-        <Provider store={reduxStore}>
-          <Header />
-            <main>
-              <Component {...pageProps} />
-            </main>
+      return { pageProps };
+   }
 
-          <style jsx>{`
-              main {
-                padding: 20px;
-              }
-          `}
-          </style>
-         </Provider>
-      </Container>
-    )
-  }
+   async componentDidMount() {
+      try {
+         // TODO: cache timestamp too.  Only get latest data.
+         const cachedToken = localStorage.getItem('in-house/token');
+
+         if (process.env.NODE_ENV !== 'development' || !this.props.reduxStore.getState().aggregate) {
+            if (cachedToken) {
+               setAuthorization(cachedToken);
+            }
+
+            const { data: aggregate } = await api.get('aggregate', { method: 'GET' });
+
+            setAuthorization(aggregate.userId);
+
+            this.props.reduxStore.dispatch(({ type: 'SET_AGGREGATE', aggregate }));
+         }
+      } catch (err) {
+         // TODO:
+         throw err;
+      }
+   }
+
+   render() {
+      const { Component, pageProps, reduxStore } = this.props;
+
+      return (
+         <Container>
+            <Provider store={reduxStore}>
+               <Header />
+               <main>
+                  <Component {...pageProps} />
+               </main>
+
+               <style jsx>{`
+                  main {
+                     padding: 20px 10px;
+                     max-width: 1200px;
+                     margin: 0 auto;
+                  }
+               `}
+               </style>
+            </Provider>
+         </Container>
+      );
+   }
 }
 
 export default withReduxStore(MyApp);
