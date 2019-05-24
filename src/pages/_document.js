ba89afe5d0a3ff30e5973132259d0756a6c1a9c8
Aleksey L <levenstein.aleksey@gmail.com>
2019-05-24 10:41:40 +0300

    fix: global styling from styled components

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 import GlobalStyle from '../components/GlobalStyle';
 
 class MyDocument extends Document {
    static async getInitialProps(ctx) {
-      const initialProps = await Document.getInitialProps(ctx);
-      return { ...initialProps };
+      const sheet = new ServerStyleSheet();
+      const originalRenderPage = ctx.renderPage;
+
+      try {
+         ctx.renderPage = () =>
+            originalRenderPage({
+               enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+            });
+
+         const initialProps = await Document.getInitialProps(ctx);
+         return {
+            ...initialProps,
+            styles: (
+               <>
+                  {initialProps.styles}
+                  {sheet.getStyleElement()}
+               </>
+            ),
+         };
+      } finally {
+         sheet.seal();
+      }
    }
 
    render() {
@@ -13,9 +34,8 @@ class MyDocument extends Document {
          <Html>
             <Head>
                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+               <link href="https://fonts.googleapis.com/css?family=Montserrat|Poppins|Roboto" rel="stylesheet" />
                <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-               <link rel="stylesheet" type="text/css" href="/static/modern-normalize.css" />
-               <link rel="stylesheet" type="text/css" href="/static/site.css" />
             </Head>
             <body>
                <GlobalStyle />
