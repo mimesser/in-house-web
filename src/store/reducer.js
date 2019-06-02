6765bfbd8dd906c363253427c62835769606618b
Aleksey L <levenstein.aleksey@gmail.com>
2019-06-02 22:43:33 +0300

    chore: remove example page

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,14 +1,12 @@
 import { combineReducers } from 'redux';
 
 import { reducer as aggregate } from './aggregate/reducer';
-import { reducer as clock } from './clock/reducer';
 import { reducer as counter } from './counter/reducer';
 import { reducer as error } from './error/reducer';
 import { reducer as venues } from './venues/reducer';
 
 export default combineReducers({
    aggregate,
-   clock,
    counter,
    error,
    venues,
--- src/store/saga.js
+++ src/store/saga.js
@@ -2,13 +2,12 @@ import { all, fork } from 'redux-saga/effects';
 import es6promise from 'es6-promise';
 
 import aggregateSaga from './aggregate/saga';
-import clockSaga from './clock/saga';
 import venuesSaga from './venues/saga';
 
 es6promise.polyfill();
 
 function* rootSaga() {
-   yield all([fork(aggregateSaga), fork(clockSaga), fork(venuesSaga)]);
+   yield all([fork(aggregateSaga), fork(venuesSaga)]);
 }
 
 export default rootSaga;
