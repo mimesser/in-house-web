b2dc39f7b1c607d7747153cef29a1399e1a7a38b
Aleksey L <levenstein.aleksey@gmail.com>
2019-06-02 22:43:33 +0300

    chore: remove example page

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,11 +1,9 @@
-import { actionTypes as clockActionTypes, startClock, tickClock } from './clock/actions';
 import { actionTypes as counterActionTypes, decrement, increment, reset } from './counter/actions';
 import { actionTypes as errorActionTypes, failure } from './error/actions';
 
 export const actionTypes = {
-   ...clockActionTypes,
    ...counterActionTypes,
    ...errorActionTypes,
 };
 
-export { startClock, tickClock, decrement, increment, reset, failure };
+export { decrement, increment, reset, failure };
