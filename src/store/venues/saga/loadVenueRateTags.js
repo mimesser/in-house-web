057cf1e303dfb9d16339819a2964fb20cd79998c
Aleksey L <levenstein.aleksey@gmail.com>
2019-06-10 18:41:27 +0300

    fix: oder rate tags according to predefined order index

--- src/store/venues/saga/loadVenueRateTags.js
+++ src/store/venues/saga/loadVenueRateTags.js
@@ -1,4 +1,5 @@
 import { call, put } from 'redux-saga/effects';
+import orderBy from 'lodash/orderBy';
 
 import api, { isForbidden } from '../../../api';
 import { clearInsiderVenue } from '../../aggregate';
@@ -8,7 +9,7 @@ import { showInsiderChallenge } from './showInsiderChallenge';
 export function* loadVenueRateTags(id) {
    try {
       const { data } = yield call(api.get, `/Venues/${id}/rateTags`);
-      yield put(setVenueRates(data));
+      yield put(setVenueRates(orderBy(data, t => t.orderIndex)));
    } catch (e) {
       if (isForbidden(e)) {
          // TODO: test when UI allows changing top mink
