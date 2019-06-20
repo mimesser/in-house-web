86453b3dc8d838902215032532e6680bfe14705b
justice <justice@in-house.com>
2019-06-20 16:00:09 +0000

    Merged PR 58: feat: add circle slider component

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,4 +1,5 @@
 export { IconInput } from './IconInput';
 export { Radio, RadioGroup } from './Radio';
+export { Slider } from './Slider';
 export { Patent } from './Patent';
 export { PokeButton } from './PokeButton';
