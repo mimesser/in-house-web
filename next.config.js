12697237c28fa64a5c7ec76ec92477fb572552d8
Aleksey L <levenstein.aleksey@gmail.com>
2019-07-12 16:00:21 +0300

    chore: env fixes

--- next.config.js
+++ next.config.js
@@ -1,39 +1,5 @@
-const { MODE } = process.env;
-
-const env = (() => {
-   switch (MODE) {
-      case 'local': {
-         return {
-            apiUrl: 'http://localhost:5080/api',
-            baseUrl: 'http://localhost:3000',
-         };
-      }
-      // If you want to test against the staging server, locally.
-      case 'local-staging': {
-         return {
-            apiUrl: 'https://inhousedev.azurewebsites.net/api',
-            baseUrl: 'http://localhost:3000',
-         };
-      }
-      case 'staging': {
-         return {
-            apiUrl: 'https://inhousedev.azurewebsites.net/api',
-            baseUrl: 'https://staging.in-house.com',
-         };
-      }
-      case 'production': {
-         return {
-            apiUrl: 'https://inhousedev.azurewebsites.net/api',
-            baseUrl: 'https://in-house.com',
-         };
-      }
-      default: {
-         console.error('Environment not supplied!');
-         return {};
-      }
-   }
-})();
-
 module.exports = {
-   env,
+   publicRuntimeConfig: {
+      MODE: process.env.MODE,
+   },
 };
