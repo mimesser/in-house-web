76a04c281c391b1351b3697c6c585b497d561e38
Aleksey L <levenstein.aleksey@gmail.com>
2019-07-17 20:26:22 +0300

    chore: fix failing tests

--- src/components/atoms/Input/test.js
+++ src/components/atoms/Input/test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Input } from '.';
 
-const wrap = (props = {}) => shallow(<Input {...props} />).dive();
+const wrap = (props = {}) => shallow(<Input {...props} />);
 
 it('renders props when passed in', () => {
    const wrapper = wrap({ type: 'text' });
