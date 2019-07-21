9be54a7644108f172983b9a445a7c8ab11f5dc5b
cuteolaf <oliverlim818@gmail.com>
2019-07-21 21:19:34 +0200

    feat: add voteCount to dial

--- src/components/organisms/Venue/RateConfirmation.js
+++ src/components/organisms/Venue/RateConfirmation.js
@@ -4,12 +4,12 @@ import { Dial } from '../../molecules';
 import { Layout, VenueTitle, ItemTitle, TeamAverage, ItemDate } from './openCardStyle';
 import { formatDate } from '../../../utils/format';
 
-export const RateConfirmation = ({ venueName, title, date, voteRating }) => (
+export const RateConfirmation = ({ venueName, title, date, voteRating, voteCount }) => (
    <Layout>
       <VenueTitle>{venueName}</VenueTitle>
       {date && <ItemDate>{formatDate(date)}</ItemDate>}
       <ItemTitle>{title}</ItemTitle>
       <TeamAverage>Team average</TeamAverage>
-      <Dial readonly value={voteRating} size={450} padd={100} inverse />
+      <Dial readonly value={voteRating} size={450} padd={100} inverse voteCount={voteCount} />
    </Layout>
 );
