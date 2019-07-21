93a77368b47ba1d43058be48c3d0d0bf3784fcb5
cuteolaf <oliverlim818@gmail.com>
2019-07-21 22:10:08 +0200

    feat: add 'you rated' text to rate-tag confirm

--- src/components/organisms/Venue/RateConfirmation.js
+++ src/components/organisms/Venue/RateConfirmation.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 import { Dial } from '../../molecules';
-import { Layout, VenueTitle, ItemTitle, TeamAverage, ItemDate } from './openCardStyle';
+import { Layout, VenueTitle, ItemTitle, SubTitle, ItemDate } from './openCardStyle';
 import { formatDate } from '../../../utils/format';
 
 export const RateConfirmation = ({ venueName, title, date, voteRating, voteCount }) => (
    <Layout>
-      <VenueTitle>{venueName}</VenueTitle>
+      <VenueTitle inverse>{venueName}</VenueTitle>
       {date && <ItemDate>{formatDate(date)}</ItemDate>}
       <ItemTitle inverse>{title}</ItemTitle>
-      <TeamAverage>Team average</TeamAverage>
+      <SubTitle>Team average</SubTitle>
       <Dial readonly value={voteRating} size={450} padd={100} inverse voteCount={voteCount} />
    </Layout>
 );
