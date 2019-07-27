40cf45eee37e8eaa3642f21081cfe72f2c4c8f02
Aleksey L <levenstein.aleksey@gmail.com>
2019-07-27 14:38:45 +0300

    fix: api base urls

--- src/settings/index.js
+++ src/settings/index.js
@@ -4,45 +4,52 @@ import getConfig from 'next/config';
 const { publicRuntimeConfig: { MODE } = {} } = getConfig() || {};
 
 const TRIAL_SUB_DOMAIN = 'demo';
-const trialApp = process.browser && window.location.host.startsWith(TRIAL_SUB_DOMAIN);
+const demoApp = process.browser && window.location.host.startsWith(TRIAL_SUB_DOMAIN);
 
 const local = {
    mainAppApi: 'http://localhost:5080/api',
-   trialAppApi: 'http://localhost:5080/api',
+   demoAppApi: 'http://localhost:5080/api',
 };
 
 const localStaging = {
-   mainAppApi: 'https://in-house-staging-admin-backend.azurewebsites.net/api',
-   trialAppApi: 'https://in-house-staging-demo-backend.azurewebsites.net/api',
-   trialApp: 'https://demo-staging.in-house.com',
+   mainAppApi: 'https://in-house-staging-backend.azurewebsites.net/api',
+   demoAppApi: 'https://in-house-staging-demo-backend.azurewebsites.net/api',
+   demoAppUrl: 'https://demo-staging.in-house.com',
+};
+
+const dev = {
+   mainAppApi: 'https://in-house-dev-backend.azurewebsites.net/api',
+   demoAppApi: 'https://in-house-dev-demo-backend.azurewebsites.net/api',
+   demoAppUrl: 'https://demo-dev.in-house.com',
 };
 
 const staging = {
-   mainAppApi: 'https://in-house-staging-admin-backend.azurewebsites.net/api',
-   trialAppApi: 'https://in-house-staging-demo-backend.azurewebsites.net/api',
-   trialApp: 'https://demo-staging.in-house.com',
+   mainAppApi: 'https://in-house-staging-backend.azurewebsites.net/api',
+   demoAppApi: 'https://in-house-staging-demo-backend.azurewebsites.net/api',
+   demoAppUrl: 'https://demo-staging.in-house.com',
 };
 
 const production = {
    mainAppApi: 'https://inhousedev.azurewebsites.net/api',
-   trialAppApi: 'https://inhousedev.azurewebsites.net/api',
-   trialApp: 'https://demo.in-house.com',
+   demoAppApi: 'https://inhousedev.azurewebsites.net/api',
+   demoAppUrl: 'https://demo.in-house.com',
    preLaunchMode: true,
 };
 
 const settingsMap = {
    local,
    localStaging,
+   dev,
    staging,
    production,
 };
 
 const settings = settingsMap[MODE] || console.error('Environment not supplied!') || {};
 
-settings.trialApp = trialApp;
+settings.demoApp = demoApp;
 Object.defineProperty(settings, 'apiUrl', {
    get() {
-      return trialApp ? this.trialAppApi : this.mainAppApi;
+      return demoApp ? this.demoAppApi : this.mainAppApi;
    },
    configurable: false,
 });
