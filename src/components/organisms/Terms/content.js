996b30e32feffa2f82c04213186bddcb2426eaa2
Aleksey L <levenstein.aleksey@gmail.com>
2019-08-13 19:37:54 +0300

    fix: terms loading [MVP-306]

--- src/components/organisms/Terms/content.js
+++ src/components/organisms/Terms/content.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const markup = { __html: require(`!raw-loader!./content.html`) };
+const markup = { __html: require(`!raw-loader!./content.html`).default };
 
 // eslint-disable-next-line react/no-danger
 export default <div dangerouslySetInnerHTML={markup} />;
