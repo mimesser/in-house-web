217bf55cdb62abc4bf1499db516b9510fee88e54
Stanley <Stanleyliu15@gmail.com>
2019-08-13 11:28:51 -0700

    chore: remove unused vars, more consistent naming

--- src/store/demo/data/minks.js
+++ src/store/demo/data/minks.js
@@ -1,4 +1,4 @@
-import { mockRate } from '../mockFunctions';
+import { mockRating } from '../mockFunctions';
 import { created } from './config';
 
 export default [
@@ -7,7 +7,7 @@ export default [
       created,
       answer: 'happyworker2020',
       question: 'what is the office wifi password? [happyworker2020]?',
-      voteRating: mockRate(),
+      voteRating: mockRating(),
       voteCount: 30,
       myCorrectAnswer: null,
       myVote: null,
@@ -17,7 +17,7 @@ export default [
       created,
       answer: 'beyonce',
       question: 'who is sergio’s muse? [beyonce]?',
-      voteRating: mockRate(),
+      voteRating: mockRating(),
       voteCount: 25,
       myCorrectAnswer: null,
       myVote: null,
@@ -27,7 +27,7 @@ export default [
       created,
       answer: 'blue',
       question: 'what’s the color of my blue suede shoes?',
-      voteRating: mockRate(),
+      voteRating: mockRating(),
       voteCount: 16,
       myCorrectAnswer: null,
       myVote: null,
--- src/store/demo/mockFunctions.js
+++ src/store/demo/mockFunctions.js
@@ -4,12 +4,12 @@ import clamp from 'lodash/clamp';
 const MIN_RATING = 1;
 const MAX_RATING = 10;
 
-export const mockRate = () => {
-   const rate = random(MIN_RATING, MAX_RATING, true);
-   return parseFloat(rate.toFixed(2));
+export const mockRating = () => {
+   const rating = random(MIN_RATING, MAX_RATING, true);
+   return parseFloat(rating.toFixed(2));
 };
 
-export const mockRateCalculate = (rate, total) => {
+export const mockCalculateRating = (rate, total) => {
    const change = random(1, true);
    const shouldIncrease = rate > 0;
    const result = shouldIncrease ? total + change : total - change;
