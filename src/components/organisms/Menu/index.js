53d37b8167eca29b77bf5c182f7244a9538b817e
Stanley <Stanleyliu15@gmail.com>
2019-08-26 11:20:12 -0700

    feat: move beta houses button [MVP-346]

--- src/components/organisms/Menu/index.js
+++ src/components/organisms/Menu/index.js
@@ -38,7 +38,11 @@ const A = styled.a`
   }
 `;
 
-const routes = [{ href: '/how-it-works', label: 'how it works' }, { href: '/feedback', label: 'feedback' }];
+const routes = [
+  { href: '/how-it-works', label: 'how it works' },
+  { href: '/feedback', label: 'feedback' },
+  { href: '/houses', label: 'see beta houses' },
+];
 
 export const Menu = withRouter(({ closeMenu, router }) => (
   <>
