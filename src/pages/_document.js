6bad3e36ba164e245a9c850d21812769d730fadf
Aleksey L <levenstein.aleksey@gmail.com>
2019-11-02 08:22:43 +0200

    feat: add metatags [DEV-489]

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -33,7 +33,33 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>In-House | Speak as a Team | Remain Untraceable</title>
+          <meta name="title" content="In-House | Speak as a Team | Remain Untraceable" />
+          <meta
+            name="description"
+            content="Challenge favoritism and politics. Promote the best ideas and people. Are you an Insider?"
+          />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://in-house.com/" />
+          <meta property="og:title" content="In-House | Speak as a Team | Remain Untraceable" />
+          <meta
+            property="og:description"
+            content="Challenge favoritism and politics. Promote the best ideas and people. Are you an Insider?"
+          />
+          <meta property="og:image" content="https://in-house.azureedge.net/webstatic/in-house-meta.png" />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://in-house.com/" />
+          <meta property="twitter:title" content="In-House | Speak as a Team | Remain Untraceable" />
+          <meta
+            property="twitter:description"
+            content="Challenge favoritism and politics. Promote the best ideas and people. Are you an Insider?"
+          />
+          <meta property="twitter:image" content="https://in-house.azureedge.net/webstatic/in-house-meta.png" />
+
           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
           <link
             href="https://fonts.googleapis.com/css?family=Roboto:300|Nunito+Sans:600|Source+Sans+Pro:300,400&display=swap"
             rel="stylesheet"
