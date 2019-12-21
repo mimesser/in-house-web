24fb14b8a1cf53ce2fda3ee20dcd6659345adfe4
Aleksey L <levenstein.aleksey@gmail.com>
2019-12-21 12:00:36 +0200

    feat: plain card

--- src/components/atoms/Card/index.js
+++ src/components/atoms/Card/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { calcRem, panelBoxShadow, palette } from '../../../style';
+import { panelBoxShadow, palette } from '../../../style';
 
 export const Card = styled.section`
   display: flex;
-  border-radius: ${calcRem('2px')};
   background-color: ${palette.white};
-  ${panelBoxShadow};
   cursor: pointer;
 `;
