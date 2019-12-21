33c065fdeecc9335b55830d6abaac9c8c28e1a30
Aleksey L <levenstein.aleksey@gmail.com>
2019-12-21 17:28:54 +0200

    chore: fix linter warnings

--- src/style/globals.js
+++ src/style/globals.js
@@ -4,4 +4,3 @@ import { appColors } from './colors';
 
 export const panelBoxShadow = 'box-shadow: 0 1px 4px 0 rgba(0,0,0,0.08);';
 export const appBackground = appColors.offWhite;
-export { appFontSize, appFontWeight, appLineHeight } from './typography';
