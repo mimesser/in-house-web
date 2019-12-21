5c9fb552d2e5ff1b655b7d243ad3dac9d99faec9
Aleksey L <levenstein.aleksey@gmail.com>
2019-12-21 12:00:36 +0200

    feat: plain card

--- src/components/atoms/Card/stories.js
+++ src/components/atoms/Card/stories.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import { Heading } from '..';
+import { H1 } from '../text';
 import { Card } from '.';
 
 storiesOf('Card', module).add('default', () => (
   <Card>
-    <Heading h3>Normal Card</Heading>
+    <H1>Plain Card</H1>
     <p>
       Nullam quis risus eget urna mollis ornare vel eu leo. Donec id elit non mi porta gravida at eget metus. Donec sed
       odio dui.
