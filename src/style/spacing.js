aafee45af9a79b81675791a0927e70d414c3aee9
Aleksey L <levenstein.aleksey@gmail.com>
2019-12-21 08:33:12 +0200

    feat: spacing variables

--- src/style/spacing.js
+++ src/style/spacing.js
@@ -2,12 +2,12 @@ import { calcRem } from './calcRem';
 
 export const spacing = {
   none: '0',
-  nano: calcRem('2px'),
-  tiny: calcRem('4px'),
-  small: calcRem('8px'),
-  medium: calcRem('12px'),
-  large: calcRem('16px'),
-  xLarge: calcRem('24px'),
-  xxLarge: calcRem('32px'),
-  xxxLarge: calcRem('40px'),
+  xxs: calcRem('2px'),
+  xs: calcRem('4px'),
+  sm: calcRem('8px'),
+  md: calcRem('12px'),
+  lg: calcRem('16px'),
+  xl: calcRem('24px'),
+  xxl: calcRem('32px'),
+  xxxl: calcRem('40px'),
 };
