de2b6fe0672277f3d7dbd4dcb2a3287f49e2c38b
Aleksey L <levenstein.aleksey@gmail.com>
2019-12-28 16:47:09 +0200

    feat: winky confirmation styles [DEV-593]

--- src/components/molecules/WinkConfirmation/index.js
+++ src/components/molecules/WinkConfirmation/index.js
@@ -5,10 +5,10 @@ import { palette } from '../../../style';
 import { Icon } from '../../atoms';
 
 export const WinkConfirmation = styled.div.attrs(props => ({
-  children: <Icon size={props.size || 15} icon="winky" />,
+  children: <Icon size={props.size || 15} icon="winky-circle" />,
 }))`
   background-color: ${palette.primary};
-  color: ${palette.white};
+  color: ${palette.mediumGray};
   flex: 1;
   display: flex;
   align-items: center;
