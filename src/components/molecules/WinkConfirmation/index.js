3d0a62a5df929de0ef23b88b08cefe28300c161f
Aleksey L <levenstein.aleksey@gmail.com>
2020-02-14 13:01:15 +0200

    fix: winky confirmation

--- src/components/molecules/WinkConfirmation/index.js
+++ src/components/molecules/WinkConfirmation/index.js
@@ -5,15 +5,8 @@ import { palette, cover } from '../../../style';
 import { Icon } from '../../atoms';
 
 export const WinkConfirmation = styled.div.attrs(props => ({
-  children: <Icon className="winky-confirmation-icon" size={props.size || 15} icon="winky-circle" />,
+  children: <Icon size={props.size || 15} icon="winky-circle" />,
 }))`
-  .winky-confirmation-icon {
-    & > svg > path {
-      fill: ${palette.primary};
-    }
-  }
-
-  // TODO:
   ${cover()};
   background-color: ${palette.primary};
   color: ${palette.mediumGray};
