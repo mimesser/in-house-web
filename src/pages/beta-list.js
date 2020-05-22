d02989f0b23d24b0f8c25c2806f37ce341303b07
mimesser <michaelmesser9507@gmail.com>
2020-05-22 22:15:10 +0300

    ui adjustments on the beta list

--- src/pages/beta-list.js
+++ src/pages/beta-list.js
@@ -1,42 +1,38 @@
 import React from 'react';
 import Link from 'next/link';
-import { Hint } from '../components/organisms/QuickListForm/Hint';
-import { QuickListForm, Page } from '../components/organisms';
+import styled from 'styled-components';
+import { Page } from '../components/organisms';
 import { BackButton, NextButton } from '../components/molecules';
 
 import { Main, Commands } from '../components/molecules/Wizard';
 import { Break, H1 } from '../components/atoms';
-import { TabTitle } from '../components/organisms/Venue/tabStyle';
 
+const Paper = styled(Page)`
+  margin-right: auto;
+  max-width: 750px;
+  padding-top: 97px;
+`;
 function BetaList() {
-  const onContact = () => {
-    console.log('contact us');
-  };
-
-  const onCancel = () => {
-    console.log('cancel us');
-  };
-
   return (
     <>
-      <Main>
-        <H1>your workspace?</H1>
-        <Break />
-        <Hint>
-          we will only be offering this platform to essential workforces during our beta trial. please contact us if you
-          like to include yours
-        </Hint>
-      </Main>
-      <Commands>
-        <Link href="/houses" passHref>
-          <BackButton secondary onClick={onCancel}>
-            cancel
-          </BackButton>
-        </Link>
-        <Link href="/feedback?subjectIndex=1">
-          <NextButton onClick={onContact}> contact us</NextButton>
-        </Link>
-      </Commands>
+      <Paper defaultHeader={false} title="In-House - List your House | Speak as a Team | Remain Untraceable">
+        <Main>
+          <H1>your workspace?</H1>
+          <Break />
+          <>
+            we will only be offering this platform to essential workforces during our beta trial. please contact us if
+            you like to include yours
+          </>
+        </Main>
+        <Commands>
+          <Link href="/houses" passHref>
+            <BackButton secondary>cancel</BackButton>
+          </Link>
+          <Link href="/feedback?subjectIndex=1">
+            <NextButton> contact us</NextButton>
+          </Link>
+        </Commands>
+      </Paper>
     </>
   );
 }
