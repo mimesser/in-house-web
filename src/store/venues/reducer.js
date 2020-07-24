98ae5b1080ba5546f4d5597e9060d17c5d92ed40
mimesser <michaelmesser9507@gmail.com>
2020-07-24 17:02:33 +0300

    fixed issue with state leaking some venue info and eventually causinga critical erro to appear

--- src/store/venues/reducer.js
+++ src/store/venues/reducer.js
@@ -114,15 +114,16 @@ export function reducer(state = initialState, action) {
 
       // TODO: consider storing items as map for easier updates
 
-      const list = (state.list && state.list.slice()) || (state.polls && state.polls.slice());
-      const index = list.findIndex((v) => v.id === venue.id);
-      list[index] = venue;
-
       const selectedVenue = state.selectedVenue && {
         ...state.selectedVenue,
+        venueInfo: { ...state.selectedVenue.venueInfo },
+        industry: { ...state.selectedVenue.industry },
         votesCount: venue.votesCount,
         rating: venue.rating,
       };
+      const list = (state.list && state.list.slice()) || (state.polls && state.polls.slice());
+      const index = list.findIndex((v) => v.id === venue.id);
+      list[index] = selectedVenue;
 
       return { ...state, list, selectedVenue };
     }
