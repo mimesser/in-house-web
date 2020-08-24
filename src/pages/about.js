f80f502e722642e29151e34c685410b19dad3ad8
Mari Taslagyan <mtaslagyan@yahoo.com>
2020-08-24 19:51:30 +0000

    Merged PR 157: Header and About Page Changes
    Updated header 'movement' to 'org 2.0'. Updated the about page title from 'about' to 'org 2.0'

--- src/pages/about.js
+++ src/pages/about.js
@@ -34,7 +34,7 @@ const Links = styled.div`
 const About = () => (
   <Page whiteHead videoBack>
     <Main>
-      <H1>about</H1>
+      <H1>Org 2.0</H1>
       <Break />
       <H2 as="p">
         we are employees, employers, management and advisors. we have seen the wars, the conspiracy theories, the
