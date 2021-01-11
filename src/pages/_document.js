33042c02c950475c8274fa1a82fd50c762aeecd0
mimesser <michaelmesser9507@gmail.com>
2021-01-11 18:07:08 +0200

    Add hotjar tracking code.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -66,6 +66,24 @@ class MyDocument extends Document {
           <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
 
           <script src={`https://www.googleoptimize.com/optimize.js?id=${process.env.G_OPTIMIZE_KEY}`} />
+
+          {process.env.HOTJAR_SITE_ID && (
+            // eslint-disable-next-line react/no-danger
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+              (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:${process.env.HOTJAR_SITE_ID},hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+              }}
+            />
+          )}
         </Head>
         <body>
           <Main />
