20c3bd45485956865327d6f497ac1f0e8bb43a41
LilFatFrank <karan.parsnani@ymail.com>
2021-03-28 20:54:08 +0530

    DEV-949 countdown clock implementation complete

--- src/components/atoms/CountDown/index.js
+++ src/components/atoms/CountDown/index.js
@@ -1,131 +1,97 @@
-import React, { useState, useEffect } from 'react';
-import moment from 'moment';
+import React from 'react';
+import { CountdownCircleTimer } from 'react-countdown-circle-timer';
 import styled from 'styled-components';
 
-const CountDownWrapper = styled.span`
+const Wrapper = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
   flex-wrap: wrap;
+  justify-content: space-around;
+  text-align: center;
+  > div {
+    margin: 0px !important;
+  }
 `;
 
-const CountDownItem = styled.span`
-  color: #fff;
-  font-size: 40px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  line-height: 30px;
-  margin: 10px;
-  padding-top: 10px;
-  position: relative;
-  width: 100px;
-  height: 100px;
-`;
-
-const CountDownSVG = styled.svg`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100px;
-  height: 100px;
+const Time = styled.div`
+  font-size: 32px;
 `;
 
 export const CountDown = (props) => {
-  const [countdownDays, setDays] = useState(undefined);
-  const [countdownHours, setHours] = useState(undefined);
-  const [countdownMinutes, setMinutes] = useState(undefined);
-  const [countdownSeconds, setSeconds] = useState(undefined);
+  const minuteSeconds = 60;
+  const hourSeconds = 3600;
+  const daySeconds = 86400;
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const { timeTillDate, timeFormat } = props;
-      const then = moment(timeTillDate, timeFormat);
-      const now = moment();
-      const countdown = moment(then - now);
-      const days = countdown.format('D');
-      const hours = countdown.format('HH');
-      const minutes = countdown.format('mm');
-      const seconds = countdown.format('ss');
-      setDays(days);
-      setHours(hours);
-      setMinutes(minutes);
-      setSeconds(seconds);
-    }, 1000);
+  const timerProps = {
+    isPlaying: true,
+    size: 120,
+    strokeWidth: 6,
+  };
 
-    return () => {
-      if (interval) {
-        clearInterval(interval);
-      }
-    };
-  }, []);
+  const renderTime = (dimension, time) => {
+    return (
+      <div>
+        <Time>{time}</Time>
+        <div>{dimension}</div>
+      </div>
+    );
+  };
 
-  function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-    const angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
+  const getTimeSeconds = (time) => (minuteSeconds - time) | 0; // eslint-disable-line no-bitwise
 
-    return {
-      x: centerX + radius * Math.cos(angleInRadians),
-      y: centerY + radius * Math.sin(angleInRadians),
-    };
-  }
+  const getTimeMinutes = (time) => ((time % hourSeconds) / minuteSeconds) | 0; // eslint-disable-line no-bitwise
 
-  function describeArc(x, y, radius, startAngle, endAngle) {
-    const start = polarToCartesian(x, y, radius, endAngle);
-    const end = polarToCartesian(x, y, radius, startAngle);
+  const getTimeHours = (time) => ((time % daySeconds) / hourSeconds) | 0; // eslint-disable-line no-bitwise
 
-    const largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1';
+  const getTimeDays = (time) => (time / daySeconds) | 0; // eslint-disable-line no-bitwise
 
-    const d = ['M', start.x, start.y, 'A', radius, radius, 0, largeArcFlag, 0, end.x, end.y].join(' ');
+  const stratTime = Date.now() / 1000; // use UNIX timestamp in seconds
+  const endTime = new Date(props.timeTillDate).getTime() / 1000; // use UNIX timestamp in seconds
 
-    return d;
-  }
+  const remainingTime = endTime - stratTime;
+  const days = Math.ceil(remainingTime / daySeconds);
+  const daysDuration = days * daySeconds;
 
-  const SVGCircle = ({ radius }) => (
-    <CountDownSVG>
-      <path fill="none" stroke="#fff" strokeWidth="4" d={describeArc(50, 50, 48, 0, radius)} />
-    </CountDownSVG>
+  return (
+    <Wrapper>
+      <CountdownCircleTimer
+        /* eslint-disable react/jsx-props-no-spreading */
+        {...timerProps}
+        colors={[['#FFF']]}
+        duration={daysDuration}
+        initialRemainingTime={remainingTime}
+      >
+        {({ elapsedTime }) => renderTime('days', getTimeDays(daysDuration - elapsedTime))}
+      </CountdownCircleTimer>
+      <CountdownCircleTimer
+        /* eslint-disable react/jsx-props-no-spreading */
+        {...timerProps}
+        colors={[['#FFF']]}
+        duration={daySeconds}
+        initialRemainingTime={remainingTime % daySeconds}
+        onComplete={(totalElapsedTime) => [remainingTime - totalElapsedTime > hourSeconds]}
+      >
+        {({ elapsedTime }) => renderTime('hours', getTimeHours(daySeconds - elapsedTime))}
+      </CountdownCircleTimer>
+      <CountdownCircleTimer
+        /* eslint-disable react/jsx-props-no-spreading */
+        {...timerProps}
+        colors={[['#FFF']]}
+        duration={hourSeconds}
+        initialRemainingTime={remainingTime % hourSeconds}
+        onComplete={(totalElapsedTime) => [remainingTime - totalElapsedTime > minuteSeconds]}
+      >
+        {({ elapsedTime }) => renderTime('minutes', getTimeMinutes(hourSeconds - elapsedTime))}
+      </CountdownCircleTimer>
+      <CountdownCircleTimer
+        /* eslint-disable react/jsx-props-no-spreading */
+        {...timerProps}
+        colors={[['#FFF']]}
+        duration={minuteSeconds}
+        initialRemainingTime={remainingTime % minuteSeconds}
+        onComplete={(totalElapsedTime) => [remainingTime - totalElapsedTime > 0]}
+      >
+        {({ elapsedTime }) => renderTime('seconds', getTimeSeconds(elapsedTime))}
+      </CountdownCircleTimer>
+    </Wrapper>
   );
-
-  /* eslint-disable camelcase */
-  function mapNumber(number, in_min, in_max, out_min, out_max) {
-    /* eslint-disable camelcase */
-    return ((number - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
-  }
-
-  const daysRadius = mapNumber(countdownDays, 30, 0, 0, 360);
-  const hoursRadius = mapNumber(countdownHours, 24, 0, 0, 360);
-  const minutesRadius = mapNumber(countdownMinutes, 60, 0, 0, 360);
-  const secondsRadius = mapNumber(countdownSeconds, 60, 0, 0, 360);
-
-  return countdownSeconds ? (
-    <>
-      <CountDownWrapper>
-        {countdownDays && (
-          <CountDownItem>
-            <SVGCircle radius={daysRadius} />
-            {countdownDays}
-          </CountDownItem>
-        )}
-        {countdownHours && (
-          <CountDownItem>
-            <SVGCircle radius={hoursRadius} />
-            {countdownHours}
-          </CountDownItem>
-        )}
-        {countdownMinutes && (
-          <CountDownItem>
-            <SVGCircle radius={minutesRadius} />
-            {countdownMinutes}
-          </CountDownItem>
-        )}
-        {countdownSeconds && (
-          <CountDownItem>
-            <SVGCircle radius={secondsRadius} />
-            {countdownSeconds}
-          </CountDownItem>
-        )}
-      </CountDownWrapper>
-    </>
-  ) : null;
 };
