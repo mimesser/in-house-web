fbaf985ac57ccfdb0a8ce6b06b8c6059ab1e36f4
Sam Morgan-Tyghe <samuelmorganvisual@gmail.com>
2021-06-27 21:12:18 +0000

    Merged PR 191: ALPHAA2-13 'Back button on feedback page'
    ALPHAA2-13 'Back button on feedback page'

--- src/components/organisms/FeedbackForm/style.js
+++ src/components/organisms/FeedbackForm/style.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 import { spacing, breakpoints } from '../../../style';
 import { Button, H2 } from '../../atoms';
+import { Icon } from '../../atoms/Icon';
 
 export const FormGroup = styled.div`
   margin: ${spacing.md} 0;
@@ -23,4 +24,15 @@ export const SubmitButton = styled(Button).attrs({
   type: 'submit',
 })`
   margin: auto 0 ${spacing.md} auto;
+`;
+
+export const BackButton = styled(Button).attrs({
+  type: 'submit',
+})`
+  margin: auto 0 ${spacing.md} 0;
+`;
+
+export const LeftArrowIcon = styled(Icon).attrs({})`
+  margin-right: 2rem;
+  margin-left: auto !important;
 `;
