9e59ee0bd8f8a15a9b27b883ae11cb89b3646a9f
Sam Morgan-Tyghe <samuelmorganvisual@gmail.com>
2021-07-11 17:57:34 +0100

    ALPHA2-62 Add back button to About Page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
 
 import { Page, HowItWorks } from '../components/organisms';
-import { H1, H2, Break } from '../components/atoms';
+import { H1, H2, Break, Button, Icon } from '../components/atoms';
 import { palette, spacing, breakpoints } from '../style';
 
 const Main = styled.div`
@@ -31,22 +32,41 @@ const Links = styled.div`
   margin: auto 0;
 `;
 
-const About = () => (
-  <Page whiteHead imageBack noPadd>
-    <Main>
-      <H1>org 2.0</H1>
-      <Break />
-      <H2 as="p">
-        we are employees, employers, management and advisors. we have seen the wars, the conspiracy theories, the
-        elephants in the room, and the skeletons in the closet, and we know that they all lead nowhere. we have seen
-        reputations, personal opportunities, and cascades of profits — all lost for nothing. we believe the family that
-        can hear each other will stop finding reasons to fear each other so we have no agenda beyond offering you this
-        tool. we use it ourselves, we have learned to listen, and it works. we believe it is time for a new day … for
-        everyone.
-        
-      </H2>
-    </Main>
-  </Page>
-);
+const BackButton = styled(Button).attrs({
+  type: 'submit',
+})`
+  margin: auto 50% ${spacing.md} 0;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
 
-export default About;
+const LeftArrowIcon = styled(Icon).attrs({})`
+  margin-right: 2rem;
+  margin-left: auto !important;
+`;
+
+export default function About() {
+  const router = useRouter();
+
+  return (
+    <Page whiteHead imageBack noPadd>
+      <Main>
+        <H1>org 2.0</H1>
+        <Break />
+        <H2 as="p">
+          we are employees, employers, management and advisors. we have seen the wars, the conspiracy theories, the
+          elephants in the room, and the skeletons in the closet, and we know that they all lead nowhere. we have seen
+          reputations, personal opportunities, and cascades of profits — all lost for nothing. we believe the family
+          that can hear each other will stop finding reasons to fear each other so we have no agenda beyond offering you
+          this tool. we use it ourselves, we have learned to listen, and it works. we believe it is time for a new day …
+          for everyone.
+        </H2>
+        <BackButton onClick={() => router.back()}>
+          <LeftArrowIcon icon="arrow-left" />
+          back
+        </BackButton>
+      </Main>
+    </Page>
+  );
+}
