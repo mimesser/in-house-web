ae5387a3b1351294df0c37028d5ea97fab38bfdc
mimesser <michaelmesser9507@gmail.com>
2021-08-12 00:50:08 +0000

    Merged PR 221: metadata & countdowntimer
    moving metadata into the official spot (_app.js)
    removing countdowntimer for now

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -29,40 +29,10 @@ class MyDocument extends Document {
     }
   }
 
-  // TODO: move meta to app after upgrading next.js
-
   render() {
     return (
       <Html>
         <Head>
-          {/* <title>In-House | Speak as a Team | Remain Untraceable</title> */}
-          {/* <meta name="title" content="In-House | Speak as a Team | Remain Untraceable" /> */}
-          <meta
-            name="description"
-            content="Challenge favoritism and politics. Promote the best ideas and people. Are you an Insider?"
-          />
-
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://in-house.com/" />
-          {/* <meta property="og:title" content="In-House | Speak as a Team | Remain Untraceable" /> */}
-          <meta
-            property="og:description"
-            content="Challenge favoritism and politics. Promote the best ideas and people. Are you an Insider?"
-          />
-          <meta property="og:image" content="https://in-house.azureedge.net/webstatic/in-house-meta.png" />
-
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content="https://in-house.com/" />
-          {/* <meta property="twitter:title" content="In-House | Speak as a Team | Remain Untraceable" /> */}
-          <meta
-            property="twitter:description"
-            content="Challenge favoritism and politics. Promote the best ideas and people. Are you an Insider?"
-          />
-          <meta property="twitter:image" content="https://in-house.azureedge.net/webstatic/in-house-meta.png" />
-
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-          <meta name="theme-color" content="#333333" />
-
           <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
 
           <script src={`https://www.googleoptimize.com/optimize.js?id=${process.env.G_OPTIMIZE_KEY}`} />
