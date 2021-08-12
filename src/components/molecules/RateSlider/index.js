076b95137c192ebfa23eb629856d12a79356124b
Sam Morgan-Tyghe <samuelmorganvisual@gmail.com>
2021-08-12 15:49:11 +0100

    add Rating Average Calculation, clean

--- src/components/molecules/RateSlider/index.js
+++ src/components/molecules/RateSlider/index.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import styled, { keyframes } from 'styled-components';
 import isNumber from 'lodash/isNumber';
+import debounce from 'lodash.debounce';
 
-import { CircleSlider, NumberLarge, NumberSmall, Icon, Slider, SlidingValue } from '../../atoms';
+import { NumberLarge, NumberSmall, Icon, Slider, SlidingValue } from '../../atoms';
 import { fontSize, font, palette, theme } from '../../../style';
 import { getClientPosition } from '../../atoms/Slider/utils';
-import { RateCategory } from '../RateCategory';
 
 const FONT_RATIO = 3.6;
 
@@ -82,6 +82,23 @@ export const Votes = styled(({ count, iconSize = 1, userRate, ...rest }) => (
   visibility: ${({ expanded }) => (expanded === true ? 'hidden' : 'visible')};
 `;
 
+const SlidingValueWrapper = styled.div`
+  width: 70px;
+  height: 54px;
+  pointer-events: none;
+  margin-top: ${({ expanded }) => (expanded === true ? '-16px' : '4px')};
+  margin-left: auto;
+  z-index: 11;
+  padding-top: 4px;
+  flex-direction: row;
+  align-content: center;
+  justify-content: space-around;
+  align-items: baseline;
+   ${({ expanded }) => (expanded === true ? 'display: flex; margin-right:50px;' : '')}
+
+}
+`;
+
 const SlidingWrapper = styled.div`
   width: 70px;
   height: 54px;
@@ -90,6 +107,8 @@ const SlidingWrapper = styled.div`
   margin-left: auto;
   z-index: 11;
   padding-top: 4px;
+
+}
 `;
 
 const Expand = keyframes`
@@ -175,7 +194,8 @@ const renderValue = (value, decimal) => {
 
 const BaseRateSlider = ({
   value: initialValue = null,
-  voteCount = 0,
+  initialRating,
+  voteCount,
   valueColor,
   title = 'rate & appreciation',
   userRate = null,
@@ -192,18 +212,56 @@ const BaseRateSlider = ({
 }) => {
   const { readonly: decimal, size, padd, fillColor = palette.darkGray } = sliderProps;
   const [value, setValue] = useState(initialValue);
+  const [hiddenValue, setHiddenValue] = useState(initialRating);
+  const [totalVoteCount, setVoteCount] = useState(voteCount);
   const [userValue, setUserValue] = useState(userRate);
+  const [tempRate, setTempRate] = useState(null);
+  const [tempClientPos, setTempClientPos] = useState(0);
+  const [preAverage, setPreAverage] = useState(null);
   const selectedRef = useRef();
   const currentValue = `${Math.floor((expanded ? userValue / 10 : value) * 10)}`;
   const changeRate = (e) => {
-    const rect = selectedRef.current.getBoundingClientRect();
     const clientPos = getClientPosition(e);
-    if (clientPos.x < 0 || clientPos.x > rect.width) return;
-    const rate = ((clientPos.x / rect.width) * 10).toFixed(1);
 
-    setUserValue(rate);
-    onChange(rate);
+    if (clientPos.x - tempClientPos > 10 || clientPos.x - tempClientPos < -10) {
+      console.log(clientPos.x - tempClientPos > 10);
+      console.log(clientPos.x - tempClientPos);
+      console.log(clientPos.x - tempClientPos);
+      console.log(clientPos.x - tempClientPos < -10);
+      setTempClientPos(clientPos.x);
+      const rect = selectedRef.current.getBoundingClientRect();
+      if (clientPos.x < 0 || clientPos.x > rect.width) return;
+      const rate = ((clientPos.x / rect.width) * 10).toFixed(1);
+
+      const rateAsNumber = Number(rate).toFixed(1);
+
+      if (rateAsNumber !== tempRate) {
+        if (rateAsNumber % 1 === 0) {
+          const newAverage = calculateMeanRating(totalVoteCount, Number(hiddenValue) || 5, Number(rate), userRate);
+          setPreAverage(newAverage);
+          onChange(rate);
+          console.log(rate);
+        }
+        setTempRate(rateAsNumber);
+        setUserValue(rate);
+      }
+    }
   };
+
+  function calculateMeanRating(nValues, average, newValue, oldValue) {
+    let trueAverage = average;
+    if (oldValue !== null) {
+      let valueA = average * nValues;
+      valueA -= oldValue;
+      const valueB = nValues - 1;
+      trueAverage = valueA / valueB;
+    }
+    const valueA = newValue - trueAverage;
+    const valueB = nValues + 1;
+    const valueC = valueA / valueB;
+    const newAverage = trueAverage + valueC;
+    return newAverage;
+  }
 
   useEffect(() => {
     setValue(initialValue);
@@ -217,38 +275,52 @@ const BaseRateSlider = ({
     <>
       <Wrapper
         ref={selectedRef}
-        // onMouseDown={!rateInProgress ? (e) => changeRate(e) : undefined}
         onMouseMove={expanded && !inProgress ? (e) => changeRate(e) : undefined}
-        onMouseUp={expanded && !inProgress ? (e) => changeRate(e) : undefined}
         onClick={expanded && !inProgress ? () => onSlideEnd(userValue) : undefined}
-        // onTouchStart={!rateInProgress ? (e) => changeRate(e) : undefined}
         onTouchMove={expanded && !inProgress ? (e) => changeRate(e) : undefined}
         onTouchEnd={expanded && !inProgress ? () => onSlideEnd(userValue) : undefined}
       >
         <Title>{title}</Title>
         <Votes count={voteCount} userRate={userRate} expanded={expanded} />
-        <SlidingWrapper expanded={expanded}>
+
+        <SlidingValueWrapper expanded={expanded}>
           {(expanded || (userValue && value)) && (
-            <SlidingValue
-              fontSize={expanded ? fontSize.lg : fontSize.md}
-              value={currentValue}
-              minLength={expanded ? 1 : 2}
-            >
-              {!expanded && <Dot size={expanded ? 140 : 80} padd={padd} color={valueColor} />}
-            </SlidingValue>
+            <>
+              <SlidingValue
+                fontSize={expanded ? fontSize.lg : fontSize.md}
+                value={
+                  // eslint-disable-next-line no-nested-ternary
+                  expanded && tempRate
+                    ? `${Math.floor(Number(tempRate).toFixed(0))}`
+                    : preAverage
+                    ? `${Math.floor(preAverage.toFixed()) * 10}`
+                    : currentValue
+                }
+                minLength={expanded ? 1 : 2}
+              >
+                {!expanded && <Dot size={expanded ? 140 : 80} padd={padd} color={valueColor} />}
+              </SlidingValue>
+              {expanded ? (
+                <>
+                  <h1>/</h1>
+
+                  <SlidingValue
+                    fontSize={expanded ? fontSize.lg : fontSize.md}
+                    value={preAverage > 0.5 ? `${preAverage.toFixed(1) * 10}` : currentValue}
+                    minLength={2}
+                  >
+                    <Dot size={expanded ? 140 : 80} padd={padd} color={valueColor} />
+                  </SlidingValue>
+                </>
+              ) : null}
+            </>
           )}
-        </SlidingWrapper>
-        {/* {isExpanded && <TouchHelper />} */}
-        <SliderWrapper expanded={expanded} duration={0.15}>
+        </SlidingValueWrapper>
+        <SliderWrapper expanded={expanded} duration={0.0015}>
           <Slider
-            // onChange={handleChange}
-            // onSlideStart={onSlideStart}
-            // onSlideEnd={onSlideEnd}
             fillColor={fillColor}
             x={(expanded && (userValue || 0.0)) || (!expanded && value)}
-            // disabled={readonly}
             selectedTag={selectedTag}
-            // inProgress={inProgress}
           >
             {userValue && !expanded && <Indicator percentage={userValue * 10} />}
           </Slider>
