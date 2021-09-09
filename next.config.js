a3a0a6b0ba5e6d69418d62409d11704ba15cfdb3
Ahmed Saleh <a.saleh.ismael@gmail.com>
2021-09-09 20:07:32 +0200

    initial sentry setup

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
+const { withSentryConfig } = require('@sentry/nextjs');
 
-module.exports = {
+const moduleExports = {
   experimental: {
     publicDirectory: true,
   },
   env: {
+    SENTRY_DSN: process.env.SENTRY_DSN,
+    SENTRY_ENVIRONMENT: process.env.SENTRY_ENVIRONMENT,
     API_URL: process.env.API_URL,
     PORT: process.env.PORT,
     GA_KEY: process.env.GA_KEY,
@@ -12,3 +15,17 @@ module.exports = {
     HOTJAR_SITE_ID: process.env.HOTJAR_SITE_ID,
   },
 };
+
+const SentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
