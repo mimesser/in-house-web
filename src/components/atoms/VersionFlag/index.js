5d7486f09fbe4eec53d3488d802ce5becb6557e9
cuteolaf <oliverlim818@gmail.com>
2021-09-29 21:17:26 -0400

    styling the versionflag a bit more

--- src/components/atoms/VersionFlag/index.js
+++ src/components/atoms/VersionFlag/index.js
@@ -5,18 +5,21 @@ import { fontSize, font, lineHeight, appColors, spacing, palette } from '../../.
 export const GlobalInfoTab = styled.div`
   display: inline;
   position: fixed;
+  vertical-align: middle;
   z-index: 1000;
   bottom: 0;
   left: ${spacing.lg};
-  padding: ${spacing.md};
-  border: 2px solid ${appColors.midnight};
-  border-top-left-radius: 25%;
-  border-top-right-radius: 25%;
+  padding: ${spacing.sm} ${spacing.md};
+  border: 3px solid ${appColors.midnight};
+  border-bottom: none;
+  border-top-left-radius: 10%;
+  border-top-right-radius: 10%;
+  transform: translateY(${spacing.sm});
   background-color: ${appColors.offWhite};
   color: ${appColors.red};
   ${font.bold}
-  line-height: ${lineHeight.md};
-  font-size: ${fontSize.lg};
+  line-height: ${lineHeight.sm};
+  font-size: ${fontSize.md};
 `;
 
 export const VersionFlag = () => <GlobalInfoTab>{`${process.env.NEXT_PUBLIC_NPM_PACKAGE_VERSION}`}</GlobalInfoTab>;
