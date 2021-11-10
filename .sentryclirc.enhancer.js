0b638f9733313f24a75222ed83f86f406f7cb83c
cuteolaf <oliverlim818@gmail.com>
2021-11-09 20:10:14 -0400

    sentry automation debug

--- .sentryclirc.enhancer.js
+++ .sentryclirc.enhancer.js
@@ -1,34 +1,38 @@
 const fs = require('fs');
 
 try {
-  let token_found = false;
-
   fs.readFile('.env.local', function (err, file) {
     if (err) throw err;
 
     const lines = file.toString().replace(/\r\n/g, '\n').split('\n');
+    const token = lines.find((l) => l.split('=')[0] === 'SENTRY_TOKEN').split('=')[1];
+    const env = lines.find((l) => l.split('=')[0] === 'ENV').split('=')[1];
 
-    for (let line of lines) {
-      const [prop, val] = line.split('=');
-
-      if (prop === 'SENTRY_TOKEN') {
-        try {
-          fs.appendFileSync('.sentryclirc', `token=${val}\n`);
-          console.log('Successfully added Sentry token to sentryrc-file!');
-        } catch (err) {
-          console.log("Whoopsie, couldn't attach sentry token to .sentryclirc!");
-          console.log(err);
-        } finally {
-          token_found = true;
-        }
-      }
-    }
-    if (!token_found) {
+    if (!token) {
       console.log('Cannot find SENTRY_TOKEN in your .env.local!!!');
+      return 1;
+    }
+
+    if (!env) {
+      console.log('Cannot find ENV variable with "localdevelopment"-value in .env.local');
+      return 2;
+    }
+
+    try {
+      if (env === 'localdevelopment') {
+        fs.appendFileSync('.sentryclirc', `token=${token}\n`);
+        console.log('Successfully added Sentry token to sentryrc-file!');
+        return 0;
+      }
+    } catch (err) {
+      console.log("Whoopsie, couldn't attach sentry token to .sentryclirc!");
+      console.log(err);
+      return 3;
     }
   });
 } catch (err) {
   console.log(
     "Couldn't read .env.local ... does it exist? You'll need it with the valid SENTRY_TOKEN entry",
   );
+  return 4;
 }
