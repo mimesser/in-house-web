3b630ba20f599374d513e21e1411eeb907b7434c
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-11-12 00:08:51 +0500

    update Tip story file

--- src/components/atoms/Tip/stories.jsx
+++ src/components/atoms/Tip/stories.jsx
@@ -2,22 +2,29 @@ import React from 'react';
 import styled from 'styled-components';
 import { storiesOf } from '@storybook/react';
 
-import { Tip } from '.';
+import { Tip } from './index';
 
-const Container = styled.div``;
+const Container = styled.div`
+  padding: 50px 60px;
+`;
 
 storiesOf('Tip', module).add('Button', () => (
   <Container>
-    <Tip tooltip="asdasdasda!" placement="bottom">
-      <span>asdasdasda</span>
+    <Tip tooltip="asdasdasda!" placement="top-start">
+      <button>Top tooltip</button>
     </Tip>
-    <Tip tooltip="Hello, World!">
-      <button>foo</button>
+    <h2>Bottom</h2>
+    <Tip tooltip="asdasdasda!" placement="bottom-end">
+      <button>Bottom tooltip</button>
     </Tip>
-    <div>
-      <Tip tooltip="Yoo!">
-        <div>dasdasda</div>
-      </Tip>
-    </div>
+    <h2>Right</h2>
+    <Tip tooltip="asdasdasda!" placement="right">
+      <button>Right tooltip</button>
+    </Tip>
+
+    <h2>Left</h2>
+    <Tip tooltip="asdasdasda!" placement="right">
+      <button>Left tooltip</button>
+    </Tip>
   </Container>
 ));
