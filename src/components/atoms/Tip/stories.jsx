2f5fdbca370d60544982745ef5d2c8193c690187
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-11-12 08:54:23 +0500

    update storybook with new categorized tooltips

--- src/components/atoms/Tip/stories.jsx
+++ src/components/atoms/Tip/stories.jsx
@@ -5,26 +5,36 @@ import { storiesOf } from '@storybook/react';
 import { Tip } from './index';
 
 const Container = styled.div`
-  padding: 50px 60px;
+  padding-top: ${(props) => props.paddingTop};
+  padding-left: ${(props) => props.paddingLeft};
 `;
 
-storiesOf('Tip', module).add('Button', () => (
-  <Container>
-    <Tip tooltip="asdasdasda!" placement="top-start">
-      <button>Top tooltip</button>
-    </Tip>
-    <h2>Bottom</h2>
-    <Tip tooltip="asdasdasda!" placement="bottom-end">
-      <button>Bottom tooltip</button>
-    </Tip>
-    <h2>Right</h2>
-    <Tip tooltip="asdasdasda!" placement="right">
-      <button>Right tooltip</button>
-    </Tip>
-
-    <h2>Left</h2>
-    <Tip tooltip="asdasdasda!" placement="right">
-      <button>Left tooltip</button>
-    </Tip>
-  </Container>
-));
+storiesOf('Tip', module)
+  .add('Top tooltip', () => (
+    <Container paddingTop="50px">
+      <Tip tooltip="asdasdasda!" placement="top-start">
+        <button>Top tooltip</button>
+      </Tip>
+    </Container>
+  ))
+  .add('Bottom tooltip', () => (
+    <Container>
+      <Tip tooltip="asdasdasda!" placement="bottom">
+        <button>Bottom tooltip</button>
+      </Tip>
+    </Container>
+  ))
+  .add('Right tooltip', () => (
+    <Container>
+      <Tip tooltip="asdasdasda!" placement="right">
+        <button>Right tooltip</button>
+      </Tip>
+    </Container>
+  ))
+  .add('Left tooltip', () => (
+    <Container paddingLeft="100px">
+      <Tip tooltip="asdasdasda!" placement="left">
+        <button>Left tooltip</button>
+      </Tip>
+    </Container>
+  ));
