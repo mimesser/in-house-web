3ddc89641b613bb87fa23ce467b32d0c14ff64d8
kristofajosh <christopherjoshua18@live.com>
2022-01-20 00:18:18 +0100

    wip

---
+++ src/components/layouts/houses.layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const HousesLayout = () => {
+  return (
+    <HousesLayoutStyling>
+      <div>top</div>
+      <div className="tabs">y</div>
+    </HousesLayoutStyling>
+  );
+};
+
+const HousesLayoutStyling = styled.section``;
+
+export default HousesLayout;
