d66ab87ed75dbdc590ce183625729d0fc4a6faed
kristofajosh <christopherjoshua18@live.com>
2022-01-23 13:05:41 +0100

    feat: create upload button

--- src/components/atoms/Button/stories.jsx
+++ src/components/atoms/Button/stories.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import { storiesOf } from '@storybook/react';
 
-import { Button as OldBtn, ClearButton } from '.';
+import { Button, ClearButton } from '.';
 import { Icon } from '../Icon';
-import Button, {BackButton, CTAButton, IconButton} from './_index';
 
 const Container = styled.div`
   display: flex;
@@ -16,17 +15,17 @@ const Container = styled.div`
   }
 `;
 
-storiesOf('Button', module).add('Button', () => (
+storiesOf('Old Button', module).add('Button', () => (
   <Container>
-    <OldBtn>primary</OldBtn>
-    <OldBtn disabled>disabled</OldBtn>
-    <OldBtn wide>wide</OldBtn>
-    <OldBtn outline>outline</OldBtn>
-    <OldBtn outline disabled>
+    <Button>primary</Button>
+    <Button disabled>disabled</Button>
+    <Button wide>wide</Button>
+    <Button outline>outline</Button>
+    <Button outline disabled>
       outline disabled
-    </OldBtn>
-    <OldBtn dashed>dashed</OldBtn>
-    <OldBtn loading>loading</OldBtn>
+    </Button>
+    <Button dashed>dashed</Button>
+    <Button loading>loading</Button>
     <ClearButton>
       clear button
       <Icon icon="arrow-right" />
@@ -34,51 +33,11 @@ storiesOf('Button', module).add('Button', () => (
     <ClearButton>
       <Icon icon="arrow-right" />
     </ClearButton>
-    <OldBtn>
+    <Button>
       with, icon <Icon icon="arrow-right" />
-    </OldBtn>
-    <OldBtn wide icon="arrow-right">
+    </Button>
+    <Button wide icon="arrow-right">
       wide, with icon as a prop
-    </OldBtn>
-  </Container>
-));
-
-storiesOf('Button', module).add('New Buttons', () => (
-  <Container>
-    <div>
-      <h1>Dark Variant</h1>
-      <Button text="default" noSuffix />
-      <br />
-      <Button text="default" noSuffix loading />
-      <Button text="default text w/icon" wide />
-      <Button text="default text w/icon" wide loading />
-      <Button outlined text="default" noSuffix />
-      <Button outlined text="default text w/icon" wide />
-      <BackButton noBorder text="default" prefix={<Icon icon="arrow-left" />} noSuffix />
-      <br />
-      <Button noBorder text="default text w/icon" />
-      <br/>
-      <IconButton icon="x"/>
-    </div>
-    <div style={{ background: '#333333', padding: 10, width: '100%' }}>
-      <h1 style={{ color: 'white' }}>Light Variant</h1>
-      <Button text="light button" variant="light" onClick={() => console.log('clicked!')} />
-      <br />
-      <Button text="default" disabled variant="light" />
-      <br />
-      <Button noBorder outline text="light" variant="light" />
-      <br/>
-      <BackButton text="default" variant="light" />
-      <br />
-      <Button outline text="default" variant="light" loading />
-      <br />
-      <Button outline text="default" variant="light" />
-      <br />
-      <Button outline text="default" variant="light" disabled />
-      <br/>
-      <IconButton icon="x" variant="light"/>
-      <br/>
-      <CTAButton text="CTA Button" />
-    </div>
+    </Button>
   </Container>
 ));
