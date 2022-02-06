2a32a6d7e96e5f9b1d60238a9d8a5bdb3dda871b
ciara.stella.azam <ciara.stella.azam@gmail.com>
2022-02-06 23:12:34 +0000

    Revert "Merged PR 383: feat: create rate venue header
    ![image.png](https://minklist.visualstudio.com/6dbd7503-de74-4334-8a67-da6a9ef2a2dd/_apis/git/repositories/a9946930-e671-4026-a85e-b0be18a97ee9/pullRequests/383/attachments/image.png)"

--- src/components/atoms/Button/_index.jsx
+++ src/components/atoms/Button/_index.jsx
@@ -212,7 +212,7 @@ export const CTAButton = (props) => (
 );
 
 export const BackButton = (props) => (
-  <Button {...props} noBorder prefix={<Icon icon="arrow-left" color="inherit" />} noSuffix />
+  <Button {...props} noBorder prefix={<Icon icon="arrow-left" />} noSuffix />
 );
 
 const IconButtonStyling = styled.button`
